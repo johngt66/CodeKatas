624e584c01f9009a3fa7818283603790bb04e68d
johngt66 <johnt@thompsonway.com>
2021-03-25 18:42:38 -0500

    version 2

--- JT.NextNumber/Program.cs
+++ JT.NextNumber/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+
 
 namespace JT.NextNumber
 {
@@ -13,36 +15,47 @@ namespace JT.NextNumber
 
                 foreach (var num in args)
                 {
-                    string nextNum = NextNumber(num);
-                    Console.WriteLine($" {num:10} -> {nextNum:10}");
+                    if (long.TryParse(num, out long n))
+                    {
+                        long nextNum = NextNumber(n);
+                        Console.WriteLine($" {n:10} -> {nextNum:10}");
+                    }
+                    else
+                        Console.WriteLine($"  Not a number: {num}");
                 }
             }
             else
             {
-                Console.WriteLine($"Enter numbers to test on command line separated by spaces.\n   $ ./{System.AppDomain.CurrentDomain.FriendlyName} 123 456 987"); 
+                Console.WriteLine($"Enter numbers to test on command line separated by spaces.\n   $ ./{System.AppDomain.CurrentDomain.FriendlyName} 123 456 987");
             }
         }
 
         // To produce the next larger number from existing digits, 
         // swap the lowest magnitude adjacent digits that are not in descending order
-        public static string NextNumber(string num)
+        public static long NextNumber(long n)
         {
-            if (!int.TryParse(num, out _))
-                return "-1";
-
-            char[] digits = num.ToCharArray().Reverse().ToArray();  // capture the digits in reverse order
-            for (var i = 0; i < digits.Length - 1; i++)             //      Compare each digit to the next
+            var num = n.ToString();
+            var digits = num.Reverse().ToList<char>();  // capture the digits in reverse order
+            IEnumerable<char> sdigits = null;
+            for (var i = 0; i < digits.Count() - 1; i++)             //      Compare each digit to the next
             {
                 if (digits[i] > digits[i + 1])                      //      Find first pair that is out of order
                 {
                     digits[i] ^= digits[i + 1];                     //      swap the digits ...
                     digits[i + 1] ^= digits[i];                     //      using old-school, memory-efficient, 
                     digits[i] ^= digits[i + 1];                     //      xor method from the assembler days.
+                    sdigits = digits.GetRange(0, i+1).OrderByDescending(c => c);
                     break;                                          //      only do this once.
                 }
             }
-            string nextNum = string.Join(string.Empty, digits.Reverse()); //    rebuild the number from the potentially rearranged digits
-            return num.Equals(nextNum, StringComparison.InvariantCultureIgnoreCase) ? "-1" : nextNum; // compare to original and return result.
+            if ((sdigits?.Count()??0) > 0)
+            {
+                digits.RemoveRange(0, sdigits.Count());
+                digits.InsertRange(0, sdigits);
+            }
+            digits.AsEnumerable().Reverse();
+            string nextNum = string.Join(string.Empty, digits.AsEnumerable().Reverse()); // rebuild the number from the potentially rearranged digits
+            return num.Equals(nextNum, StringComparison.InvariantCultureIgnoreCase) ? -1 : long.Parse(nextNum); // compare to original and return result.
         }
     }
 }
