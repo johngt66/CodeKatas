0f98d374e24de17acfb24a6ffa2198467981e06e
johngt66 <johnt@thompsonway.com>
2021-03-25 19:35:50 -0500

    refactoring

--- JT.NextNumber.Test/JT.NextNumber_Test.cs
+++ JT.NextNumber.Test/JT.NextNumber_Test.cs
@@ -9,9 +9,27 @@ namespace JT.NextNumber.Test
         [TestMethod]
         public void NextNumber_OnSuccessReturnsNextNumber()
         {
-            string num = "513";
-            string expected = "531";
-            string actual = Program.NextNumber(num);
+            long num = 513;
+            long  expected = 531;
+            long actual = Program.NextNumber(num);
+            Assert.AreEqual(expected,actual);
+        }
+        [TestMethod]
+        public void NextNumber_Test2()
+        {
+            long num = 1234567890;
+            long expected = 1234567908;
+            long actual = Program.NextNumber(num);
+            Assert.AreEqual(expected,actual);
+        }
+        
+        [TestMethod]
+        public void NextNumber_Test3()
+        {
+            long num = 59884848489553;
+            long expected = 59884848483559;
+            long other =    59884848493558;
+            long actual = Program.NextNumber(num);
             Assert.AreEqual(expected,actual);
         }
     }
