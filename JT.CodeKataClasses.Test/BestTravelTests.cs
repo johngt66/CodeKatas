4b19db7e19fbd5358ba41e7ca309f633fdf5abb8
John Thompson <johnt@thompsonway.com>
2021-04-19 23:16:37 -0500

    Completed Best Travel

--- JT.CodeKataClasses.Test/BestTravelTests.cs
+++ JT.CodeKataClasses.Test/BestTravelTests.cs
@@ -1,25 +1,46 @@
 using System;
-using NUnit.Framework;
 using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-[TestFixture]
-public class SumOfKTests {
+[TestClass]
+public class SumOfKTests
+{
 
-[Test]
-  public void Test1() {
-    Console.WriteLine("****** Basic Tests");
-    List<int> ts = new List<int> {50, 55, 56, 57, 58};
-    int? n = SumOfK.chooseBestSum(163, 3, ts);
-    Assert.AreEqual(163, n);
+    [TestMethod]
+    public void Test1()
+    {
+        Console.WriteLine("****** Basic Tests");
+        List<int> ts = new List<int> { 50, 55, 56, 57, 58 };
+        int? n = SumOfK.chooseBestSum(163, 3, ts);
+        Assert.AreEqual(163, n);
+    }
+    [TestMethod]
+    public void Test2()
+    {
+        List<int> ts = new List<int> { 50 };
+        int? n = SumOfK.chooseBestSum(163, 3, ts);
+        Assert.AreEqual(null, n);
+    }
+    [TestMethod]
+    public void Test3()
+    {
+        List<int> ts = new List<int> { 91, 74, 73, 85, 73, 81, 87 };
+        int? n = SumOfK.chooseBestSum(230, 3, ts);
+        Assert.AreEqual(228, n);
+    }
+    [TestMethod]
+    public void test0()
+    {
+        List<int> ls = new List<int> { 1, 2, 3, 4, 5, 6, 7 };
+        ls = ls.Select((x, i) => check(x, i)).ToList();
+        Assert.AreEqual(0,0);
+    }
 
-    ts = new List<int> {50};
-    n = SumOfK.chooseBestSum(163, 3, ts);
-    Assert.AreEqual(null, n);
-
-    ts = new List<int> {91, 74, 73, 85, 73, 81, 87};
-    n = SumOfK.chooseBestSum(230, 3, ts);
-    Assert.AreEqual(228, n);
-  }
+    private int check(int x, int i)
+    {
+        Console.WriteLine(i);
+        return x;
+    }
 }
--- JT.CodeKataClasses/BestTravel.cs
+++ JT.CodeKataClasses/BestTravel.cs
@@ -1,16 +1,24 @@
-
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class SumOfK
 {
     public static int? chooseBestSum(int t, int k, List<int> ls)
     {
-        return ProcessTravel(new List<int>(), t, k, ls);
-    }
+        if (ls.Count() < k) return null;
+        if (k == 1) return ls.Max(m => m > t ? null : m as int?);
 
-    private static int? ProcessTravel(List<int> included, int t, int k, List<int> ls)
-    {
-        included.Add
+        int? best = null;
+        for (int i = 0; i < ls.Count; i++)
+        {
+            if (chooseBestSum(t - ls[i], k - 1, ls.Skip(i + 1).ToList()) is int s)
+            {
+                s += ls[i];
+                if (s <= t && s > (best ?? 0))
+                    best = s;
+            }
+        }
+        return best;
     }
 }
